dca11586b007a9136b5d42588233eb4695fcde4f
mukade <ericfugao@163.com>
2014-11-28 16:01:26 +0800

    11281601

--- Java1200/src/exp_028_LeapYear.java
+++ Java1200/src/exp_028_LeapYear.java
@@ -12,16 +12,16 @@ public class exp_028_LeapYear {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		while (true){
+			System.out.println("please enter a year");
 			long year = scan.nextLong();
 			String str = new String();
-			System.out.println("please enter a year");
 			if(year%4==0&&year%100!=0&&year%400==0){
 				System.out.println(year+"is leap year");
 			}else{
 				System.out.println(year+"is not leap year");
 			}
-			System.out.println("press any key to continue or press bye to quit");
-			str = scan.nextLine();
+			System.out.println("press any key to continue or press bye to quit");System.out.println();
+			str = scan.nextLine();System.out.print("hi");
 			if (str == "bye")break;
 		}
 	}
