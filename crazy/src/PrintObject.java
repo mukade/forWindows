01533c84fc8e989ac4f363bcc597c6dfbce239e3
mukade <ericfugao@163.com>
2014-12-03 00:20:59 +0800

    12030020

---
+++ crazy/src/PrintObject.java
@@ -0,0 +1,23 @@
+
+class Person {
+	private String name;
+	
+	public Person(String name)
+	{
+		this.name = name;
+	}
+	
+	public void info(){
+		System.out.println("此人名为："+name);
+	}
+	
+	public String toString(){
+		return this.name;
+	}
+}
+ public class PrintObject{
+	 public static void main(String args[]){
+		 Person p = new Person("孙悟空");
+		 System.out.println(p);
+	 }
+ }
---
+++ crazy/src/StringCompareTest.java
@@ -0,0 +1,15 @@
+public class StringCompareTest {
+	public static void main(String args[]){
+		String s1 = "·è¿ñjava";
+		String s2 = "·è¿ñ";
+		String s3 = "java";
+		String s4 = "·è¿ñ"+"java";
+		String s5 = "·è"+"¿ñ"+"java";
+		String s6 = s2+s3;
+		String s7 = new String("·è¿ñjava");
+		System.out.println(s1 == s4);
+		System.out.println(s1 == s5);
+		System.out.println(s1 == s6);
+		System.out.println(s1 == s7);
+	}
+}
