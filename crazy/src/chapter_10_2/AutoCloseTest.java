e2624814d41f1d4d164f22cebdd4f003748ef45c
mukade <ericfugao@163.com>
2015-03-04 17:13:24 +0800

    15-03-04 17:13

---
+++ crazy/src/chapter_10_2/AutoCloseTest.java
@@ -0,0 +1,19 @@
+package chapter_10_2;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintStream;
+
+public class AutoCloseTest {
+	public static void main(String args[]) throws Exception{
+		try(	//
+				BufferedReader br=new BufferedReader(new FileReader("AutoCloseTest.java"));
+				PrintStream ps=new PrintStream(new FileOutputStream("a.txt"));
+				//声明、初始化两个可关闭资源
+			){
+			System.out.println(br.readLine());
+			ps.println("asdffdsa");
+		}
+	}
+}
